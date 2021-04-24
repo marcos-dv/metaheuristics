508408a699bbe54cf83c01cd08ffa3c63d11bf7c
Marcos <marcosdominguez.ampl@gmail.com>
2021-04-24 22:23:42 +0200

    Renamed kbest algorithms
    New kbest method

--- src/utils/Pair.java
+++ src/utils/Pair.java
@@ -20,19 +20,38 @@ public class Pair implements Comparable<Pair> {
 	    return Double.valueOf(this.value).compareTo(other.value);
 	}
 	
-	public static Pair[] getKMinimum(Pair[] pairs) {
+	public static Pair[] sortIdxPairs(Pair[] pairs) {
+		if (pairs == null) {
+			System.out.println("Warning-Pair: getKMinimum pairs are not initialized");
+		}
 		Pair[] kMin = pairs.clone();
 		Arrays.sort(kMin);
 		return kMin;
 	}
 
-	public static Pair[] getKBest(Solution[] sols) {
+	public static Pair[] sortIdxSolutions(Solution[] sols) {
+		if (sols == null) {
+			System.out.println("Warning-Pair: getKBest solutions are not initialized");
+		}
+			
 		Pair kbest[] = new Pair[sols.length];
 		for(int i = 0; i < sols.length; ++i) {
+			if (sols[i] == null)
+				continue;
 			kbest[i] = new Pair(i, sols[i].getFitness());
 		}
-		Arrays.sort(kbest);
+		return sortIdxPairs(kbest);
+	}
+	
+	public static Solution[] getKbest(Solution [] sols, int k) {
+		Pair[] idx = sortIdxSolutions(sols);
+		Solution[] kbest = new Solution[k];
+		for(int i = 0; i < k; ++i) {
+			int kIndex = idx[i].index;
+			kbest[i] = new Solution(sols[kIndex]);
+		}
 		return kbest;
 	}
+
 
 }
