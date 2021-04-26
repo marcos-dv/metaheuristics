2398c4e2a2ddf998b5ba1f9fa905da85d5fa6bf1
Marcos <marcosdominguez.ampl@gmail.com>
2021-04-26 23:15:23 +0200

    Updating EW matrix and analyze landscape

--- src/metaheuristics/PTAlgorithm.java
+++ src/metaheuristics/PTAlgorithm.java
@@ -4,18 +4,23 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 import problems.Problem;
+import problems.Solution;
 import utils.Globals;
 
 public abstract class PTAlgorithm {
 	private IMetaheuristic metaheuristic;
 	private double [] paramRange;
 	private double [] meansEW;
+	private double [] prevFitness;
 	private double [][] EW;
 	private Problem problem;
 	private int popsize;
 
 	private boolean DEBUG = true;
 	private double curAlfa; // current value for the parameter alfa
+	
+	private double SIGN_TOLERANCE = 0;
+	private double DELTA_THRESHOLD = 1e-7;
 	
 	public PTAlgorithm(IMetaheuristic metaheuristic) {
 		this.metaheuristic = metaheuristic;
@@ -92,6 +97,8 @@ public abstract class PTAlgorithm {
 				EW[i][j] = 1;
 			meansEW[i] = calcMean(i);
 		}
+		// Initialize previous fitness for each iteration
+		prevFitness = null;
 	}
 
 	// Return the index of the best param
@@ -134,8 +141,108 @@ public abstract class PTAlgorithm {
 		return mean / (double)popsize;
 	}
 	
-	protected void updateEW(int row) {
-		
+	public void initPrevFitness() {
+		// Only for first iteration, initialize prev fitness
+		if (prevFitness == null) {
+			Solution[] sols = metaheuristic.getSols();
+			if(popsize != sols.length) {
+				System.out.println("Warning-PTAlgorithm: popsize != sols.length in metaheuristic!\n\tpopsize = " 
+						+ popsize + " and sols.length = " + sols.length);
+			}
+			prevFitness = new double[popsize];
+			for(int i = 0; i < popsize; ++i) {
+				prevFitness[i] = sols[i].getFitness();
+			}
+		}
+	}
+	
+	public void updateCaseAllGood(int row, double [] delta) {
+		for(int j = 0; j < popsize; ++j) {
+			EW[row][j] = Math.tanh(delta[j]/popsize);
+		}
+	}
+
+	public void updateCaseAllBad(int row, double [] delta) {
+		// Negative weights
+		for(int j = 0; j < popsize; ++j) {
+			EW[row][j] = Math.tanh(delta[j]/popsize);
+		}
+	}
+
+	public void updateCaseMixed(int row, double [] delta) {
+		for(int j = 0; j < popsize; ++j) {
+			EW[row][j] = Math.abs(Math.tanh(delta[j]/popsize));
+		}
+	}
+
+	public void updateCaseStagnition(int row, double [] delta) {
+		for(int j = 0; j < popsize; ++j) {
+			EW[row][j] = -2;
+		}
+	}
+
+	public void updateRow(int row, double [] delta) {
+		assert(popsize == delta.length);
+		int cntPlus = 0;
+		int cntMinus = 0;
+		int cntZero = 0;
+		int cntStagnition = 0;
+
+		// SIGN_TOLERANCE ~ 0
+		// Analysis of the landscape
+		for(int i = 0; i < popsize; ++i) {
+			// Magnitude
+			if (Math.abs(delta[i]) < DELTA_THRESHOLD) {
+				cntStagnition++;
+			}
+			// Sign
+			if (delta[i] > SIGN_TOLERANCE) {
+				cntPlus++;
+			}
+			else if (delta[i] < -SIGN_TOLERANCE) {
+				cntMinus++;
+			}
+			else if (Math.abs(delta[i]) <= SIGN_TOLERANCE) {
+				cntZero++;
+			}
+		}
+		// Update
+		// Case 6 -> Stagnition
+		if (cntStagnition == popsize) {
+			updateCaseStagnition(row, delta);
+		}
+		// Cases 1 and 2, improvement
+		else if (cntMinus == 0) {
+			updateCaseAllGood(row, delta);
+		}
+		// Cases 3 and 4, bad improvement
+		else if (cntPlus + cntZero == 0) {
+			updateCaseAllBad(row, delta);
+		}
+		// Cases 5, exploration
+		else if ((cntPlus + cntZero > 0) && (cntMinus > 0)) {
+			updateCaseMixed(row, delta);
+		}
+		else {
+			System.out.println("Warning-PTAlgorithm: Unknown landscape case");
+		}
+	}
+	
+	
+	public void updateEW(int row) {
+		// first iteration
+		if (prevFitness == null) {
+			initPrevFitness();
+		}
+		else {
+			double [] deltaFitness = new double[popsize];
+			Solution [] sols = metaheuristic.getSols();
+			// Compute differences
+			for(int i = 0; i < popsize; ++i) {
+				deltaFitness[i] = sols[i].getFitness() - prevFitness[i];
+			}
+			updateRow(row, deltaFitness);
+		}
 	}
 	
 	public void nextIter() {
