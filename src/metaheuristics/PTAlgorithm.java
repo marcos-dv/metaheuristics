bd4db7df89fa6e6dfb5dd0a1c304eafff2f5a01d
Marcos <marcosdominguez.ampl@gmail.com>
2021-04-26 22:19:39 +0200

    Added EW and means calculation

--- src/metaheuristics/PTAlgorithm.java
+++ src/metaheuristics/PTAlgorithm.java
@@ -1,5 +1,6 @@
 package metaheuristics;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import problems.Problem;
@@ -8,6 +9,7 @@ import utils.Globals;
 public abstract class PTAlgorithm {
 	private IMetaheuristic metaheuristic;
 	private double [] paramRange;
+	private double [] meansEW;
 	private double [][] EW;
 	private Problem problem;
 	private int popsize;
@@ -67,28 +69,72 @@ public abstract class PTAlgorithm {
 	public void setPopsize(int popsize) {
 		this.popsize = popsize;
 	}
-
+	
 	public void setup() {
 		metaheuristic.initPop();
 		if (paramRange == null) {
 			System.out.println("Warning-PTAlgorithm: range of the parameter is not initialized");
 		}
+		// Initialize EW matrix and means
 		EW = new double[paramRange.length][popsize];
+		meansEW = new double[paramRange.length];
+		for(int i = 0; i < paramRange.length; ++i) {
+			for(int j = 0; j < popsize; ++j)
+				EW[i][j] = 1;
+			meansEW[i] = calcMean(i);
+		}
 	}
 
+	// Return the index of the best param
 	public int selectParamValue() {
-		// Random pick
-		return Globals.getRandomGenerator().randomInt(0, paramRange.length);
+		double bestMean = Double.NEGATIVE_INFINITY;
+		ArrayList<Integer> bestIndexes = new ArrayList<>();
+		for(int i = 0; i < paramRange.length; ++i) {
+			// better value
+			if (meansEW[i] > bestMean) {
+				bestIndexes.clear();
+				bestIndexes.add(i);
+				bestMean = meansEW[i];
+			}
+			// candidate
+			else if (meansEW[i] == bestMean) {
+				bestIndexes.add(i);
+			}
+		}
+		if (bestIndexes.size() == 0) {
+			System.out.println("Warning-PTAlgorithm: not best parameter value found");
+		}
+		if (DEBUG) {
+			System.out.println("# Best indexes and values:");
+			for (Integer index: bestIndexes) {
+				System.out.println(index + " -> " + paramRange[index]);
+			}
+		}
+		int idx = Globals.getRandomGenerator().randomInt(0, bestIndexes.size());
+		return bestIndexes.get(idx);
 	}
 	
 	// Update in the metaheuristic the proper parameter!
 	public abstract void setNewParam(double alfa);
+	
+	public double calcMean(int row) {
+		double mean = 0;
+		for(int j = 0; j < popsize; ++j) {
+			mean += EW[row][j];
+		}
+		return mean / (double)popsize;
+	}
+	
+	protected void updateEW(int row) {
+		
+	}
 	
 	public void nextIter() {
 		int idx = selectParamValue();
 		double alfa = paramRange[idx];
 		setNewParam(alfa);
 		metaheuristic.nextIter();
+		updateEW(idx);
 	}
 
 	public void run(int numiter) {
--- src/tests/TestPTGSA.java
+++ src/tests/TestPTGSA.java
@@ -20,9 +20,21 @@ public class TestPTGSA {
 		ptgsa.run(maxiter);		
 	}
 	
+	public static void testUpdateEW(int dim, int popsize, int maxiter) {
+		// Fix seed for experiments
+		Globals.getRandomGenerator().setSeed(1);
+	
+		Problem problem = new MinSqSumProblem(dim);
+		// popsize
+		GSA gsa = new GSA(popsize, problem);
+		double [] alfas = {2., 5., 7.};
+		PTGSA ptgsa = new PTGSA(gsa, alfas);
+		ptgsa.run(maxiter);		
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Start test");
-		testSetParam(5, 3, 5);
+		testSetParam(5, 3, 50);
 		System.out.println("End testing");
 	}
 
