60e00adb1627df5c0d02553a320c08ad3156d7f5
Marcos <marcosdominguez.ampl@gmail.com>
2021-04-27 04:55:20 +0200

    TestFuncCec2015 cleaned

--- src/exec/TestCec2015.java
+++ src/exec/TestCec2015.java
@@ -5,11 +5,8 @@ import java.util.Scanner;
 import utils.testfunc;
 
 public class TestCec2015 {
-	public static void main(String[] args) throws Exception{
-		double[] OShift,M,y,z,x_bound;
-		int ini_flag = 0,n_flag,func_flag;
-		int[] SS;
-		
+
+	public static void testmain() throws Exception {
 		int i,j,k,n,m,func_num;
 		double[] f,x;
 		File fpt;
@@ -54,10 +51,50 @@ public class TestCec2015 {
 					System.out.println("f"+func_num+"(x["+(j+1)+"])="+f[j]);
 				}
 			}
+			input.close();
+		}
+	}
+
+	public static void mytestmain() throws Exception {
+		int i,j,k,n,m,func_num;
+		double[] f,x;
+		File fpt;
+		
+		m=5;
+		n=30;
+		
+		x = new double[m*n];
+		f = new double[m];
+		
+		testfunc tf = new testfunc();
+		 
+		for (i=0;i<15;i++)
+		{
+			func_num = i+1;
+			fpt = new File("input_data/shift_data_"+func_num+".txt");
+			Scanner input = new Scanner(fpt);
+			
+			for(k=0;k<n;k++)
+			{
+				x[k] = input.nextDouble();
+			}
 			
 			input.close();
 			
+			for (k=0;k<1;k++)
+			{
+				tf.test_func(x,f,n,m,func_num);
+				for(j=0;j<m;j++)
+				{
+					System.out.println("f"+func_num+"(x["+(j+1)+"])="+f[j]);
+				}
+			}
+			input.close();
 		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		mytestmain();
 		
 		
 	}
