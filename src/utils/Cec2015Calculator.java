ba31e4849fa004b19ca17e276361c428de499580
Marcos <marcosdominguez.ampl@gmail.com>
2021-05-23 18:35:12 +0200

    Added Cec2015Calculator class
    It is an extension of FitnessCalculator which evaluates the multiple solutions at a time only for Cec problems

---
+++ src/utils/Cec2015Calculator.java
@@ -0,0 +1,31 @@
+package utils;
+
+import problems.Cec2015Problem;
+import problems.Problem;
+import solutions.Solution;
+
+public class Cec2015Calculator extends FitnessCalculator {
+
+	public Cec2015Calculator(int i0, int incr_i, Solution[] sols, Problem problem, double[] f) {
+		super(i0, incr_i, sols, problem, f);
+	}
+	
+	@Override
+	public void run() {
+		Solution [] subSols = new Solution[incr_i];
+		for(int i = 0; i < incr_i; ++i) {
+			subSols[i] = sols[i+firstIdx];
+		}
+		Cec2015Problem problem = (Cec2015Problem) this.problem;
+		try {
+			double[] fit = problem.contestFitnessMultipleSols(subSols);
+			for(int i = 0; i < incr_i; ++i) {
+				f[i+firstIdx] = fit[i];
+			}
+		} catch (Exception e) {
+			System.out.println("Error-Cec2015Calculator: multiple solutions evaluation did not work.");
+		}
+	}
+
+	
+}
--- src/utils/FitnessCalculator.java
+++ src/utils/FitnessCalculator.java
@@ -5,17 +5,17 @@ import solutions.Solution;
 
 public class FitnessCalculator extends Thread {
 
-	int i0;
-	int incr_i;
-	Solution [] sols;
-	Problem problem;
-	double [] f;
-	int totalTasks;
+	protected int firstIdx;
+	protected int incr_i;
+
+	protected Solution [] sols;
+	protected Problem problem;
+	protected double [] f;
 	
 	public FitnessCalculator(int i0, int incr_i, Solution [] sols, Problem problem, double[] f) {
 		this.sols = sols;
 		this.problem = problem;
-		this.i0 = i0;
+		this.firstIdx = i0;
 		this.incr_i = incr_i;
 		this.f = f;
 		// System.out.println("Start at " + i0 + " End at " + (i0+incr_i-1));
@@ -23,10 +23,49 @@ public class FitnessCalculator extends Thread {
 	
 	@Override
 	public void run() {
-		for(int i = i0; i < i0+incr_i && i < sols.length; ++i) {
+		for(int i = firstIdx; i < firstIdx+incr_i && i < sols.length; ++i) {
 			double fit = problem.fitness(sols[i]);
 			f[i] = fit;
 		}
 	}
 
+	public Problem getProblem() {
+		return problem;
+	}
+
+	public void setProblem(Problem problem) {
+		this.problem = problem;
+	}
+
+	public int getIncr_i() {
+		return incr_i;
+	}
+
+	public void setIncr_i(int incr_i) {
+		this.incr_i = incr_i;
+	}
+
+	public Solution[] getSols() {
+		return sols;
+	}
+
+	public void setSols(Solution[] sols) {
+		this.sols = sols;
+	}
+
+	public double[] getF() {
+		return f;
+	}
+
+	public void setF(double[] f) {
+		this.f = f;
+	}
+
+	public int getFirstIndex() {
+		return firstIdx;
+	}
+
+	public void setFirstIndex(int i0) {
+		this.firstIdx = i0;
+	}
 }
